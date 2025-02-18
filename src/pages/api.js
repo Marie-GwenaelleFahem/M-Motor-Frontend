bdc5593e7bd1f69f3675b82871ad0180571ca202
Meryem2510 <meryem18elfelloussi@gmail.com>
2025-02-18 21:38:59 +0100

    fixed the cors access control , database succesfully connected

--- src/pages/api.js
+++ src/pages/api.js
@@ -2,10 +2,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // URL de backend
+  baseURL: "http://localhost:8000",
+  //  // URL de backend
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
   },
+
 });
 
 export default api;
